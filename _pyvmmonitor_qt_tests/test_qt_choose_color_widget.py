2a5e6e9230ac5123ae710a2abf27af0274f88804
fabioz <fabiofz@gmail.com>
2017-10-08 15:47:16 -0300

    Color wheel selector showing current color selection.

--- _pyvmmonitor_qt_tests/test_qt_choose_color_widget.py
+++ _pyvmmonitor_qt_tests/test_qt_choose_color_widget.py
@@ -4,25 +4,25 @@ from pyvmmonitor_qt.pytest_plugin import qtapi  # @UnusedImport
 def test_choose_color_widget(qtapi):
     from pyvmmonitor_qt.qt_choose_color_widget import ChooseColorWidget
     from pyvmmonitor_qt.qt_event_loop import process_events
-    from pyvmmonitor_core.math_utils import almost_equal
-    color_widget = ChooseColorWidget()
-    color_widget.show()
+    from pyvmmonitor_qt.qt.QtGui import QColor
+    choose_color_widget = ChooseColorWidget()
+    choose_color_widget.show()
 
     process_events()
-    color_wheel_widget = color_widget.color_wheel_widget
-    color_widget.color_wheel_widget.repaint()
-    size = color_widget.color_wheel_widget._pixmap.size()
+    color_wheel_widget = choose_color_widget.color_wheel_widget
+    choose_color_widget.color_wheel_widget.repaint()
+    pixmap_size = choose_color_widget.color_wheel_widget._pixmap.size()
+
+    choose_color_widget.set_color(QColor(255, 0, 0))
 
     # Just checking that the color wheel was built on paint
-    assert size.width() > 30
-    assert size.height() > 30
-    assert color_wheel_widget.width() == size.width()
-    assert color_wheel_widget.height() == size.height()
+    assert pixmap_size.width() > 30
+    assert pixmap_size.height() > 30
+    assert color_wheel_widget.width() > pixmap_size.width()
+    assert color_wheel_widget.height() > pixmap_size.height()
 
-    assert color_wheel_widget.saturation_from_point(size.width() / 2., size.height() / 2.) == 0.0
-    assert color_wheel_widget.saturation_from_point(size.width(), 0.0) == 1.0
-    assert almost_equal(
-        color_wheel_widget.saturation_from_point(size.width() / 4., size.height() / 2.), 0.5)
+    center = color_wheel_widget._center
 
-    assert color_wheel_widget.hue_from_point(size.width(), size.height() / 2) == 0.0
-    assert color_wheel_widget.hue_from_point(size.width() / 2., size.height()) == 0.25
+    assert color_wheel_widget.saturation_from_point(center[0], center[1]) == 0.0
+    assert color_wheel_widget.saturation_from_point(pixmap_size.width(), 0.0) == 1.0
+
--- pyvmmonitor_qt/qt_choose_color_widget.py
+++ pyvmmonitor_qt/qt_choose_color_widget.py
@@ -8,27 +8,32 @@ from pyvmmonitor_qt.qt_app import obtain_qapp
 logger = logging.getLogger(__name__)
 
 
-class _ColorWheelWidget(QWidget):
+class _QPixmapWidget(QWidget):
 
     def __init__(self, *args, **kwargs):
-        from pyvmmonitor_core.callback import Callback
         super().__init__(*args, **kwargs)
-
         self._pixmap = None
-        # Called with on_hue_saturation_selected(hue, saturation)
-        # 0 <= hue <= 1
-        # 0 <= saturation <= 1
-        self.on_hue_saturation_selected = Callback()
+        self._last_size = None
 
-    def paintEvent(self, *args, **kwargs):
+    def paintEvent(self, ev):
         from pyvmmonitor_qt.qt_utils import painter_on
-        if self._pixmap is None or (
-                self._pixmap.width(), self._pixmap.height()) != (self._w, self._h):
+        if self._pixmap is not None:
+            size = self._pixmap.width(), self._pixmap.height()
+
+        if self._pixmap is None or size != self._last_size:
             self._create_pixmap()
 
+            if self._pixmap is None:
+                logger.critical('Pixmap not created in: %s' % (self,))
+                self._last_size = None
+                return
+
+            size = self._pixmap.width(), self._pixmap.height()
+            self._last_size = size
+
         with painter_on(self, False) as painter:
-            painter.drawPixmap(0, 0, self._pixmap)
-        return
+            diff = (self._w - size[0]) / 2, (self._h - size[1]) / 2
+            painter.drawPixmap(int(diff[0]), int(diff[1]), self._pixmap)
 
     @property
     def _w(self):
@@ -42,6 +47,72 @@ class _ColorWheelWidget(QWidget):
     def _center(self):
         return self.width() / 2, self.height() / 2
 
+
+class _ColorWheelWidget(_QPixmapWidget):
+
+    def __init__(self, *args, **kwargs):
+        from pyvmmonitor_core.callback import Callback
+        super().__init__(*args, **kwargs)
+
+        # Called with on_hue_saturation_selected(hue, saturation)
+        # 0 <= hue <= 1
+        # 0 <= saturation <= 1
+        self.on_hue_saturation_selected = Callback()
+        self._last_pos = None
+        self._color = None
+
+    @property
+    def _wheel_size(self):
+        return self._w - int(self._pointer_size * 1.3)
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, color):
+        '''
+        :param QColor color:
+        '''
+        self._color = color
+        self.update()
+
+    @property
+    def _pointer_size(self):
+        pointer_size = int(self._w * .03) + 1
+        if pointer_size % 2 == 0:
+            pointer_size += 1
+        return pointer_size
+
+    def paintEvent(self, ev):
+        from pyvmmonitor_qt.qt_utils import painter_on
+        from pyvmmonitor_qt.qt.QtCore import Qt
+        from pyvmmonitor_core import math_utils
+        import math
+
+        _QPixmapWidget.paintEvent(self, ev)
+        color = self._color
+        if color is None:
+            return
+        hue, saturation = color.hsvHueF(), color.hsvSaturationF()
+        size = self._pixmap.width()
+        center = self._center
+        degrees = hue * 360
+        distance = saturation * (size / 2)
+
+        pointer_size = self._pointer_size
+        delta = pointer_size / 2
+
+        pt = math_utils.create_point(center, math.radians(degrees), distance)
+        rect = pt[0] - delta, pt[1] - delta, pointer_size, pointer_size
+        rect2 = rect[0] - 1, rect[1] - 1, rect[2] + 2, rect[3] + 2
+
+        with painter_on(self, True) as painter:
+            painter.setPen(Qt.white)
+            painter.drawEllipse(*rect)
+            painter.setPen(Qt.black)
+            painter.drawEllipse(*rect2)
+
     def _create_pixmap(self):
         hue_pixmap = self._create_hue_pixmap()
         self._pixmap = self._lighten_with_alpha(hue_pixmap)
@@ -54,11 +125,13 @@ class _ColorWheelWidget(QWidget):
         from pyvmmonitor_qt.qt.QtGui import QRadialGradient
         from pyvmmonitor_qt.qt.QtGui import QBrush
 
+        size = self._wheel_size
+
         # Create a gradient to lighten up the center
-        alpha_channel_pixmap = QPixmap(self._w, self._h)
+        alpha_channel_pixmap = QPixmap(size, size)
         alpha_channel_pixmap.fill(Qt.transparent)
-        radius = (self._w / 2) - 4
-        rg = QRadialGradient(self._w / 2, self._h / 2, radius, self._w / 2, self._h / 2)
+        radius = (size / 2) - 4
+        rg = QRadialGradient(size / 2, size / 2, radius, size / 2, size / 2)
 
         delta = 0.1
         v = 0.0
@@ -69,7 +142,7 @@ class _ColorWheelWidget(QWidget):
         with painter_on(alpha_channel_pixmap, True) as painter:
             painter.setPen(Qt.NoPen)
             painter.setBrush(QBrush(rg))
-            painter.drawEllipse(0, 0, self._w, self._h)
+            painter.drawEllipse(0, 0, size, size)
 
         with painter_on(hue_pixmap, True) as painter:
             # Draw the alpha channel pixmap on top of the hue.
@@ -91,13 +164,13 @@ class _ColorWheelWidget(QWidget):
         stops = 360
         delta = 1.0 / stops
         v = 0.0
-        w, h = self._w, self._h
+        size = self._wheel_size
 
-        pixmap = QPixmap(w, h)
+        pixmap = QPixmap(size, size)
         pixmap.fill(Qt.transparent)
 
         degrees = 360
-        gradient = QConicalGradient(w / 2, h / 2, degrees)
+        gradient = QConicalGradient(size / 2, size / 2, degrees)
         for _stop in range(stops):
             v += delta
             # we do 1.0 - v to have blue on top (just a matter of taste really).
@@ -107,43 +180,59 @@ class _ColorWheelWidget(QWidget):
         with painter_on(pixmap, True) as painter:
             painter.setPen(Qt.NoPen)
             painter.setBrush(QBrush(gradient))
-            painter.drawEllipse(0, 0, w, h)
+            painter.drawEllipse(0, 0, size, size)
 
         return pixmap
 
     def saturation_from_point(self, x, y):
         from pyvmmonitor_core.math_utils import calculate_distance
         point = x, y
-        w, h = self._w, self._h
+        size = self._wheel_size
         center = self._center
 
         distance = calculate_distance(center, point)
         if distance == 0.0:
             return 0.0
-        if w != h:
-            logger.warn('Hue may be wrong when w (%s) != h (%s)' % (w, h))
 
-        max_distance = w / 2
+        max_distance = size / 2
         if distance >= max_distance:
             return 1.0
         return distance / max_distance
 
     def hue_from_point(self, x, y):
         from pyvmmonitor_core import math_utils
-        import math
         point = x, y
         angle = math_utils.calc_angle_in_radians(self._center, point)
         degrees = math_utils.radians_to_0_360_degrees(angle)
         return degrees / 360.
 
+    def _on_mouse_pos(self, pos):
+        self._last_pos = pos
+        saturation = self.saturation_from_point(*pos)
+        hue = self.hue_from_point(*pos)
+        self.on_hue_saturation_selected(hue, saturation)
+
     def mousePressEvent(self, ev):
         from pyvmmonitor_qt.qt.QtCore import Qt
         if ev.button() == Qt.LeftButton:
             pos = ev.pos()
-            saturation = self.saturation_from_point(pos.x(), pos.y())
-            hue = self.hue_from_point(pos.x(), pos.y())
-            self.on_hue_saturation_selected(hue, saturation)
+            pos = pos.x(), pos.y()
+            self._on_mouse_pos(pos)
+
         return QWidget.mousePressEvent(self, ev)
+
+    def mouseMoveEvent(self, ev):
+        if self._last_pos is not None:
+            pos = ev.pos()
+            pos = pos.x(), pos.y()
+            self._last_pos = pos
+            self._on_mouse_pos(pos)
+
+        return QWidget.mouseMoveEvent(self, ev)
+
+    def mouseReleaseEvent(self, ev):
+        self._last_pos = None
+        return QWidget.mouseReleaseEvent(self, ev)
 
 
 class _SelectedColorWidget(QWidget):
@@ -170,8 +259,8 @@ class _SelectedColorWidget(QWidget):
             with painter_on(self, False) as painter:
                 radius = self.width() / 5
                 painter.setBrush(self._color)
-                w = self.width() - 2
-                painter.drawRoundedRect(0, 0, w, w, radius, radius)
+                size = self.width() - 2
+                painter.drawRoundedRect(0, 0, size, size, radius, radius)
         else:
             super().paintEvent(ev)
 
@@ -199,14 +288,18 @@ class ChooseColorWidget(QWidget):
             self._on_hue_saturation_selected)
 
     def _on_hue_saturation_selected(self, hue, saturation):
-        print('update', hue, saturation)
         from pyvmmonitor_qt.qt.QtGui import QColor
         color = QColor.fromHsvF(hue, saturation, 1.0)
-        self._selected_color_widget.color = color
+        self.set_color(color)
 
     @property
     def color_wheel_widget(self):
         return self._color_wheel_widget
+
+    def set_color(self, qcolor):
+        self._color = qcolor
+        self._selected_color_widget.color = qcolor
+        self._color_wheel_widget.color = qcolor
 
 if __name__ == '__main__':
     qapp = obtain_qapp()
